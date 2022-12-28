42f8b1798f9bf7182150207271c000945d1f175c
Kaushal.Kumar <kaushal140798@gmail.com>
2022-12-28 20:49:09 +0530

    get request with header

--- front-end/src/Components/products/ProductList.js
+++ front-end/src/Components/products/ProductList.js
@@ -1,23 +1,37 @@
-import React, { useEffect, useState, axios } from 'react'
+import axios from 'axios'
+import React from 'react'
+import { useEffect, useState } from 'react'
 
 const ProductList = () => {
-    const [products, setProducts,] = useState([])
+    const [myData, setData] = useState([])
+    const token = JSON.parse(localStorage.getItem('user')).auth;
+
+    const config = {
+        headers: {
+            'content-type': 'multipart/form-data',
+            'authorization': token
+        },
+    };
 
     useEffect(() => {
-        getProducts()
+        axios.get("http://localhost:5000/getAllProducts", config)
+            .then((response) => setData(response.data))
     }, [])
 
-    const auth = localStorage.getItem('user')
-
-    const getProducts = async () => {
-        let result = await fetch('http://localhost:5000/getAllProducts')
-        result = await result.json()
-        setProducts(result)
-
-        console.warn("products")
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -31,3 +45,13 @@ const ProductList = () => {
 }
 
 export default ProductList
+
+
+
+{/*  const getProducts = async () => {
+        let result = await fetch('http://localhost:5000/getAllProducts')
+        result = await result.json()
+        setProducts(result)
+
+        console.warn("products")
+    } */}
