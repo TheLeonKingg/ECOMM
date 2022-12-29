be6f7ab135d1108006bf50523eb88e97020801ab
Kaushal.Kumar <kaushal140798@gmail.com>
2022-12-29 10:19:45 +0530

    product list complete

--- front-end/src/Components/products/ProductList.js
+++ front-end/src/Components/products/ProductList.js
@@ -1,57 +1,48 @@
 import axios from 'axios'
 import React from 'react'
 import { useEffect, useState } from 'react'
+import "./productlist.css"
 
 const ProductList = () => {
     const [myData, setData] = useState([])
     const token = JSON.parse(localStorage.getItem('user')).auth;
-
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data',
-            'authorization': token
-        },
-    };
+    const config = { headers: { 'authorization': token }, };
 
     useEffect(() => {
         axios.get("http://localhost:5000/getAllProducts", config)
             .then((response) => setData(response.data))
     }, [])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return (
-        <div>ProductList</div>
+        <div className='product-list'>
+            <h3> ProductList</h3>
+            <ul>
+                <li>S.No</li>
+                <li>Image</li>
+                <li>Name</li>
+                <li>price</li>
+                <li>Category</li>
+            </ul>
+
+            {
+                myData.map((item, index) =>
+                    <ul key={item}>
+                        <li>{index + 1}</li>
+                        <li>{item.image}</li>
+                        <li>{item.name}</li>
+                        <li>{item.price}</li>
+                        <li>{item.category}</li>
+                    </ul>
+                )
+            }
+
+
+
+
+
+        </div>
     )
 }
 
 export default ProductList
 
-
-
-{/*  const getProducts = async () => {
-        let result = await fetch('http://localhost:5000/getAllProducts')
-        result = await result.json()
-        setProducts(result)
-
-        console.warn("products")
-    } */}
